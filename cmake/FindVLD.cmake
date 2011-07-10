9f403b93b54715943b2e161c2b855e51d59b7c89
Sergiu Deitsch <sergiu.deitsch@gmail.com>
2011-07-10 16:42:57 +0200

    updated vld cmake module

--- cmake/FindVLD.cmake
+++ cmake/FindVLD.cmake
@@ -1,5 +1,5 @@
-# Module for locating the Visual Leak Detector.
-# 
+# Module for locating Visual Leak Detector.
+#
 # Cutomizable variables:
 #   VLD_ROOT_DIR
 #     This variable points to the Visual Leak Detector root directory. By
@@ -10,18 +10,19 @@
 #   VLD_FOUND
 #     Indicates that the library has been found.
 #
-#   VLD_INCLUDE_DIR
+#   VLD_INCLUDE_DIRS
 #     Points to the Visual Leak Detector include directory.
+#
+#   VLD_LIBRARY_DIRS
+#     Points to the Visual Leak Detector directory that contains the libraries.
+#     The content of this variable can be passed to link_directories.
 #
 #   VLD_LIBRARIES
 #     Points to the Visual Leak Detector libraries that can be passed to
 #     target_link_libararies.
 #
-#   VLD_LIBRARIES_DIR
-#     Points to the Visual Leak Detector directory that contains the libraries.
-#     The content of this variable can be passed to link_directories.
 #
-# Copyright (c) 2010 Sergiu Dotenco
+# Copyright (c) 2010, 2011 Sergiu Dotenco
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -66,39 +67,42 @@ FIND_PATH (VLD_ROOT_DIR
   PATHS ${_VLD_POSSIBLE_DIRS}
   DOC "VLD root directory")
 
-FIND_PATH (VLD_INCLUDE_DIR 
+FIND_PATH (VLD_INCLUDE_DIR
   NAMES vld.h
   PATHS ${VLD_ROOT_DIR}
   PATH_SUFFIXES ${_VLD_POSSIBLE_INCLUDE_SUFFIXES}
   DOC "VLD include directory")
 
-FIND_PATH (VLD_LIBRARIES_DIR
-  NAMES vld.lib
+IF (VLD_INCLUDE_DIR)
+  SET (VLD_INCLUDE_DIRS ${VLD_INCLUDE_DIR})
+ENDIF (VLD_INCLUDE_DIR)
+
+FIND_LIBRARY (VLD_LIBRARY_DEBUG
+  NAMES vld
   PATHS ${VLD_ROOT_DIR}
   PATH_SUFFIXES ${_VLD_POSSIBLE_LIB_SUFFIXES}
-  DOC "VLD libraries directory")
+  DOC "VLD debug library")
 
-IF (MSVC)
-  FIND_LIBRARY (VLD_LIBRARIES_DEBUG
-    NAMES vld
-    PATHS ${VLD_ROOT_DIR}
-    PATH_SUFFIXES ${_VLD_POSSIBLE_LIB_SUFFIXES}
-    DOC "VLD libraries")
+IF (VLD_LIBRARY_DEBUG)
+  SET (VLD_LIBRARIES debug ${VLD_LIBRARY_DEBUG} CACHE STRING "VLD libraries")
+  GET_FILENAME_COMPONENT (_VLD_LIBRARY_DIR ${VLD_LIBRARY_DEBUG} PATH)
+  SET (VLD_LIBRARY_DIR ${_VLD_LIBRARY_DIR} CACHE PATH "VLD library directory")
+  SET (VLD_LIBRARY_DIRS ${VLD_LIBRARY_DIR})
+ENDIF (VLD_LIBRARY_DEBUG)
 
-  SET (VLD_LIBRARIES debug ${VLD_LIBRARIES_DEBUG})
-ENDIF (MSVC)
+MARK_AS_ADVANCED (VLD_LIBRARY_DIR VLD_LIBRARY_DEBUG)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS (VLD DEFAULT_MSG
-  VLD_INCLUDE_DIR VLD_LIBRARIES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS (VLD REQUIRED_VARS VLD_INCLUDE_DIRS
+  VLD_LIBRARIES VLD_LIBRARY_DIRS)
 
 IF (NOT VLD_FOUND)
   IF (NOT PACKAGE_FIND_QUIETLY)
     IF (PACKAGE_FIND_REQUIRED)
-      MESSAGE (FATAL_ERROR 
+      MESSAGE (FATAL_ERROR
         "VLD required but some files were not found. "
-        "Specify the VLD location using VLD_ROOT_DIR")
+        "Specify VLD's location using VLD_ROOT_DIR.")
     ENDIF (PACKAGE_FIND_REQUIRED)
   ENDIF (NOT PACKAGE_FIND_QUIETLY)
 ENDIF (NOT VLD_FOUND)
 
-MARK_AS_ADVANCED (VLD_INCLUDE_DIR VLD_LIBRARIES VLD_LIBRARIES_DIR)
+MARK_AS_ADVANCED (VLD_ROOT_DIR VLD_INCLUDE_DIR VLD_LIBRARIES)
